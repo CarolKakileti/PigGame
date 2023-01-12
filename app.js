b828feee224f65fc4b9a3573b311594779157254
Carol Kakileti <carolkakileti1@gmail.com>
2023-01-13 00:30:00 +0530

    Adding scores to CurrentScore

--- app.js
+++ app.js
@@ -5,7 +5,8 @@
  -The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
  -The first player to reach 100 points on GLOBAL score wins the game.
  */
-var scores, roundScore, activePlayer, gamePlaying;
+
+ var scores, roundScore, activePlayer, gamePlaying;
 
 scores = [0,0];
 roundScore = 0;
@@ -13,18 +14,37 @@ activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContext = '0';
+document.getElementById('score-1').textContext = '0';
+document.getElementById('current-0').textContext = '0';
+document.getElementById('current-1').textContext = '0';
 
-document.querySelector('.btn-roll').addEventListener('click', function(){
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
     
     //1. Random number
-    dice = Math.floor(Math.random() * 6) + 1;
+    var dice = Math.floor(Math.random() * 6) + 1;
 
     //2. Display the result
     var diceDOM = document.querySelector('.dice');
-    document.querySelector('.dice').style.display = 'block';
+    diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
 
     //3. Update the round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).tectContent = roundScore;
+    } else {
+        //Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+    }
+    
+
+
+
 
 });
 
@@ -33,4 +53,4 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
 //document.querySelector('#current-' + activePlayer).tectContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-//var x = document.querySelector('#score-0').textContent;
+//var x = document.querySelector('#score-0').textContent;`
